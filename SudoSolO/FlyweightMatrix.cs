73df2ecb30d63586bc699ce93d3f509bf0c59079
imressed <imressed@gmail.com>
2013-04-22 20:51:01 +0300

    flyweight for matrix

---
+++ SudoSolO/FlyweightMatrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudoSolO
+{
+    abstract class FlyweightMatrix
+    {
+        protected int size;
+        abstract public int[] GetRow(int row);
+        abstract public int[] GetColumn(int column);
+        abstract public int[] GetRegion(int row, int column);
+        //abstract public void SetField(int[,] array);
+    }
+}
